51ef2f294d29fd5946342c78c6895409d8334c33
Thomas Paul Mann <hi@thomaspaulmann.com>
2016-11-07 19:06:44 +0100

    Add basic API

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,23 @@
-print("Hello, world!")
+import Commander
+
+Group {
+    $0.command("init") {
+        print("Init Swish...")
+    }
+
+    $0.command("build", Flag("clean", description: "Makes a clean build.")) { clean in
+        if clean {
+            print("Clean build Swish...")
+        } else {
+            print("Build Swish...")
+        }
+    }
+
+    $0.command("run") {
+        print("Run Swish...")
+    }
+
+    $0.command("stop") {
+        print("Stop Swish...")
+    }
+}.run()
